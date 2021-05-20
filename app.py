f1f829ebd02df437eeeb9e3371d6191fac989e49
Joseph Donaldson <41586191+mrseph34@users.noreply.github.com>
2021-05-19 22:52:23 -0400

    dodo
    do

--- app.py
+++ app.py
@@ -5,31 +5,47 @@ import requests
 import sqlite3 as sql
 
 app = Flask(__name__, static_folder="static")
-#comment
-#comment2
-con = sql.connect("data.db")
+
+con = sql.connect("static/data/data.db")
 cur = con.cursor()
 
-cur.execute('CREATE TABLE IF NOT EXISTS "users" ("username"  TEXT, "password"  TEXT)')
-
+cur.execute('CREATE TABLE IF NOT EXISTS "users" ("name" TEXT, "email" TEXT, "password"  TEXT)')
+cur.execute('SELECT email FROM users')
 print(cur.fetchall())
 
-cur.close
+cur.close()
 
 @app.route('/')
 def index():
  
     return render_template('index.html')
 
-@app.route('/create',methods = ['POST', 'GET'])
-def login():
+@app.route('/register',methods = ['POST', 'GET'])
+def register():
+    con = sql.connect("static/data/data.db")
+    cur = con.cursor()
     name = request.form['name']
     email = request.form['email']
     password = request.form['password']
 
-   
+   # cur.execute("INSERT INTO users(name,email,password) VALUES((?),(?),(?))", (name, email, password))
 
-    return render_template('create.html', password = password, email = email, name = name)
+    cur.execute('SELECT email FROM users')
+
+    emails = cur.fetchall()
+    print(cur.fetchall())
+    huh = "EMAIL DO NOT EXIST"
+
+    for row in emails:
+        if row == email:
+            huh = "EMAIL EXISTS"
+        
+    #if huh == False:
+       # cur.execute("INSERT INTO users(name,email,password) VALUES((?),(?),(?))", (name, email, password))
+
+    cur.close()
+
+    return render_template('index.html', email = email, name = name, created = huh)
 
 
     
